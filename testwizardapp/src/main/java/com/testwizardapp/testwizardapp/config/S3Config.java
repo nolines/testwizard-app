a5a10f85eda4b6b0f60134025ae8387906d89849
umurcan <5785630+umurcanemre@users.noreply.github.com>
2022-10-29 15:50:51 +0200

    s3 config externalized

--- testwizardapp/src/main/java/com/testwizardapp/testwizardapp/config/S3Config.java
+++ testwizardapp/src/main/java/com/testwizardapp/testwizardapp/config/S3Config.java
@@ -1,5 +1,6 @@
 package com.testwizardapp.testwizardapp.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
@@ -9,11 +10,19 @@ import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
 public class S3Config {
+
+    // Investigate possible string pooling related vulnerabilities
+    @Value("${filestorage.s3.accessKey}")
+    private String accessKey;
+
+    @Value("${filestorage.s3.secretKey}")
+    private String secretKey;
+
     @Bean(destroyMethod = "close")
     public S3Client s3Client() {
         return S3Client.builder()
                 .region(Region.of("eu-central-1"))
-                .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create("AKIA56X4C2LWWWHCUHNI", "6L6pzsyqv4/68US4aQCvT0641A5Y34sVNJIm+3i1")))
+                .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey)))
                 .build();
     }
 }
