827343e5e8625d790f90a0a6306eb73dc414ac58
umurcan <5785630+umurcanemre@users.noreply.github.com>
2022-11-15 20:25:49 +0100

    tests fixed to pass

--- testwizardapp/src/test/java/com/testwizardapp/testwizardapp/TestwizardappApplicationTests.java
+++ testwizardapp/src/test/java/com/testwizardapp/testwizardapp/TestwizardappApplicationTests.java
@@ -1,9 +1,12 @@
 package com.testwizardapp.testwizardapp;
 
+import org.junit.ClassRule;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = "spring.mongodb.embedded.version=3.5.5")
 class TestwizardappApplicationTests {
 
     @Test
